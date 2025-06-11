fd44c97dcc1cc8730711f16dd8ec48e84ebca5df
Sean Fisher <srtfisher@gmail.com>
2025-06-11 11:57:01 -0400

    Drop the need for Composer

--- wp-404-caching.php
+++ wp-404-caching.php
@@ -18,36 +18,9 @@
 namespace Alley\WP\WP_404_Caching;
 
 use Alley\WP\WP_404_Caching\Features\Full_Page_Cache_404;
-use Composer\InstalledVersions;
-use function add_action;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
-}
-
-// Check if Composer is installed (remove if Composer is not required for your plugin).
-if ( ! file_exists( __DIR__ . '/vendor/wordpress-autoload.php' ) ) {
-	// Will also check for the presence of an already loaded Composer autoloader
-	// to see if the Composer dependencies have been installed in a parent
-	// folder. This is useful for when the plugin is loaded as a Composer
-	// dependency in a larger project.
-	if ( ! class_exists( InstalledVersions::class ) ) {
-		add_action(
-			'admin_notices',
-			function () {
-				?>
-				<div class="notice notice-error">
-					<p><?php esc_html_e( 'Composer is not installed and wp-404-caching cannot load. Try using a `*-built` branch if the plugin is being loaded as a submodule.', 'wp-404-caching' ); ?></p>
-				</div>
-				<?php
-			}
-		);
-
-		return;
-	}
-} else {
-	// Load Composer dependencies.
-	require_once __DIR__ . '/vendor/wordpress-autoload.php';
 }
 
 /**
